1256f7442a218493efb6a73a9ed629a869f635be
Srija-Akula <121477807+Srija-Akula@users.noreply.github.com>
2026-01-12 21:05:31 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,3 +5,20 @@ DATABASES = {
         default=os.environ.get('DATABASE_URL')
     )
 }
+
+
+## installed apps
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'users',
+    'posts',
+    'friends',
+    'notifications',
+    'api',
+]
