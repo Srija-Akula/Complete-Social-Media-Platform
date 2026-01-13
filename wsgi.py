f9f87d0ffc64f30dd056b4fef8bcba504e36163f
Srija-Akula <121477807+Srija-Akula@users.noreply.github.com>
2026-01-13 20:32:04 +0530

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+## wsgi.py
+
+"""
+WSGI config for social_platform project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_platform.settings')
+
+application = get_wsgi_application()
